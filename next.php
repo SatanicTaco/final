3b98b19a59634c8ac146dc88d2c4bf8a5c146236
SatanicTaco <55203131+SatanicTaco@users.noreply.github.com>
2019-12-16 11:12:06 -0500

    ver2

--- next.php
+++ next.php
@@ -22,7 +22,7 @@
                     VALUES ('{$search}', '{$ip}')";
                 //confirmation
                 if (mysqli_query($conn, $sql)) {
-                    header('Location: https://www.google.ca/$search');
+                    header('Location: https://www.google.ca/',$search);
                 } else {
                     echo "Error: " . $sql . "<br>" . mysqli_error($conn);
                 }
